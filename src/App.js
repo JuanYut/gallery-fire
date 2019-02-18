e62606bb19c60100174551dfee1c343ccd99e721
JuanSantillan97 <juanyut97@hotmail.com>
2019-02-18 14:59:51 -0600

    subir img al storage y login con Google

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import firebase from 'firebase'
+import FileUpload from './FileUpload'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      user: null
+    }
+    this.handleAuth = this.handleAuth.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user }) // user: user
+    })
+  }
+
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase.auth().signInWithPopup(provider)
+    .then(result => console.log(`${result.user.email} ha iniciado sesion.`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  handleLogout() {
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user.email} ha cerrado sesion.`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
+  renderLoginButton() {
+    if(this.state.user) {
+      // Si esta logueado.
+      return(
+        <div className="App-intro">
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+          <p className="App-intro">Hola { this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout} className="App-btn">Bye bye</button>
+          <FileUpload />
+        </div>
+      )
+    } else {
+      // Si no esta logueado.
+      return(
+        <div>
+          <button onClick={this.handleAuth} className="App-btn">Iniciar sesion con Google</button>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h2>Gallery-Fire</h2>
+        </div>
+        { this.renderLoginButton() }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAz7VVnrLnNACEYIHAW4sRvBr9sxuDOsog",
+  authDomain: "gallery-fire-f26af.firebaseapp.com",
+  databaseURL: "https://gallery-fire-f26af.firebaseio.com",
+  projectId: "gallery-fire-f26af",
+  storageBucket: "gallery-fire-f26af.appspot.com",
+  messagingSenderId: "426131609935"
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
